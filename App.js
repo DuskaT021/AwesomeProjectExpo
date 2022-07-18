7ece9120665e689ed533ffb87d04e38c580a0fd3
DuskaT021 <aleksa2601@gmail.com>
2022-07-19 01:01:20 +0200

    fixing eslint errors

--- App.js
+++ App.js
@@ -1,29 +1,26 @@
 import 'react-native-gesture-handler';
-import React from "react";
+import React from 'react';
 // import { View, Text, SafeAreaView, StyleSheet, FlatList } from 'react-native';
 // import ColorBox from "./components/ColorBox";
 import Home from './screens/Home';
 import ColorPalette from './screens/ColorPalette';
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
-
 const App = () => {
   return (
-
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name="Home" component={Home} />
         <Stack.Screen
-          name='ColorPalette'
+          name="ColorPalette"
           component={ColorPalette}
           options={({ route }) => ({ title: route.params.paletteName })}
         />
       </Stack.Navigator>
     </NavigationContainer>
-
-  )
+  );
 };
 export default App;
